528ed158ce952373a1daf58a6f3c77730aaf84cf
Arafat <arafatahmedtanimcsedu57@gmail.com>
2025-05-01 01:52:25 +0600

    update

--- app/(home)/_components/central-system.tsx
+++ app/(home)/_components/central-system.tsx
@@ -1,30 +1,30 @@
-"use client";
+'use client';
 
-import { useInView } from "react-intersection-observer";
-import { SectionTitle } from "@/components/shared/section-title";
+import { useInView } from 'react-intersection-observer';
+import { SectionTitle } from '@/components/shared/section-title';
 
 export const CentralSystem = () => {
-  const { ref, inView } = useInView({
-    triggerOnce: true, // only run once
-    threshold: 0.2, // % of visibility before triggering
-  });
+	const { ref, inView } = useInView({
+		triggerOnce: true, // only run once
+		threshold: 0.2, // % of visibility before triggering
+	});
 
-  return (
-    <section className="my-16" ref={ref}>
-      <div className="container mx-auto flex flex-col gap-16">
-        <SectionTitle title="Everything you need to secure code, cloud, and runtime– in one central system" />
+	return (
+		<section className="my-16" ref={ref}>
+			<div className="container mx-auto flex flex-col gap-16">
+				<SectionTitle title="Everything you need to secure code, cloud, and runtime– in one central system" />
 
-        <div className="px-4">
-          {inView && (
-            <object
-              type="image/svg+xml"
-              data="/assets/home/section-03/content-with-bg-and-pic.svg"
-            >
-              svg-animation
-            </object>
-          )}
-        </div>
-      </div>
-    </section>
-  );
+				<div className="px-4">
+					{inView && (
+						<object
+							type="image/svg+xml"
+							data="/assets/home/section-03/content-with-bg-and-pic.svg"
+						>
+							svg-animation
+						</object>
+					)}
+				</div>
+			</div>
+		</section>
+	);
 };
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,34 +1,34 @@
-import { HeroSection } from "./_components/HeroSection";
-import { Intro } from "./_components/intro";
-import { CentralSystem } from "./_components/central-system";
-import { FlowAnalysis } from "./_components/FlowAnalysis";
-import { TeamDown } from "./_components/team-down";
-import { Section06Summarized } from "./_components/section-06-summarized";
-import { Section07Integrations } from "./_components/section-07-integrations";
-import { Section09Graphic } from "./_components/section-09-graphic";
-import { Section10Articles } from "./_components/section-10-articles";
+import { HeroSection } from './_components/HeroSection';
+import { Intro } from './_components/intro';
+import { CentralSystem } from './_components/central-system';
+import { FlowAnalysis } from './_components/FlowAnalysis';
+import { TeamDown } from './_components/team-down';
+import { Section06Summarized } from './_components/section-06-summarized';
+import { Section07Integrations } from './_components/section-07-integrations';
+import { Section09Graphic } from './_components/section-09-graphic';
+import { Section10Articles } from './_components/section-10-articles';
 
 export default function Home() {
-  return (
-    <div className="flex flex-col relative">
-      {/* Hero Section */}
-      <HeroSection />
+	return (
+		<div className="flex flex-col relative">
+			{/* Hero Section */}
+			<HeroSection />
 
-      <Intro />
+			<Intro />
 
-      <CentralSystem />
+			<CentralSystem />
 
-      <FlowAnalysis />
+			<FlowAnalysis />
 
-      <TeamDown />
+			<TeamDown />
 
-      {/*<Section06Summarized />
+			<Section06Summarized />
 
-      <Section07Integrations />
+			<Section07Integrations />
 
-      <Section09Graphic />
+			<Section09Graphic />
 
-      <Section10Articles /> */}
-    </div>
-  );
+			{/*<Section10Articles /> */}
+		</div>
+	);
 }
